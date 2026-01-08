05fdba9c9fff239c90bdf76055649a5464655cc6
GeetanjaliVaidya <74186515+GeetanjaliVaidya@users.noreply.github.com>
2026-01-08 20:00:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const toggle = document.getElementById('toggle');
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('light');
+  document.body.classList.toggle('dark');
+});
